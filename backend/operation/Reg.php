a2dfc1bc49bdcef8917e7f0a749a97fc0910a0cc
adeojoemmanuel <emmanuel.adeojo@yahoo.com>
2018-09-30 08:12:56 +0100

    backend for leap

---
+++ backend/operation/Reg.php
@@ -0,0 +1,48 @@
+<?php
+	session_start();
+	require('./../util/functions.php');
+	require_once ('./../util/hashing.php');
+	$utility = new Utility();
+	$auth = new Auth;
+	$fields = array('reg_no', 'mat_no', 'password',  'email', 'dept', 'category');
+	$error = false; 
+
+	foreach($fields AS $fieldname) { 
+	  if(!isset($_POST[$fieldname]) || empty($_POST[$fieldname])) {
+	    echo 'Field '.$fieldname.' missing!<br />';
+	    $error = true;
+	  }
+	}
+	$field = array('reg_no', 'mat_no', 'password',  'email', 'dept', 'category' 'act_code');
+	if(!$error) {
+		$reg_no = $utility->clean_input($_POST['reg_no']);
+		$mat_no = $utility->clean_input($_POST['mat_no']);
+		$password = $utility->clean_input($_POST['password']);
+		$email =  $utility->clean_input($_POST['email']);
+		$dept = $utility->clean_input($_POST['dept']);
+		$category = $utility->clean_input($_POST['category']);
+		$act_code = $auth->random_char();
+		$act_status = 0;
+		if($password == $password){
+			$password = passwordHash::hash($password);
+			if($utility->validate_email($email)) {
+				$values = array('reg_no' => $reg_no, 'mat_no' => $mat_no,  'password' => $password, 'email' => $email,  'dept' => $dept, 'category' => $category, 'act_code'=>$act_code);
+				$auth = new Auth();
+				$main = $auth->register('participant', $field, $values, $act_code);
+			}else {
+				$_SESSION['message'] = "Wrong email format";
+				$_SESSION['messagetype'] ="alert alert-danger";
+				$utility->redirect('./../../index.php');
+			}
+		}else{
+			$_SESSION['message'] = "password not match";
+			$_SESSION['messagetype'] ="alert alert-danger";
+			$utility->redirect('./../../index.php');
+		}
+	}else{
+		$_SESSION['message'] = "input error";
+		$_SESSION['messagetype'] ="alert alert-danger";
+		$utility->redirect('./../../index.php');
+	}
+
+?>
---
+++ backend/util/config.php
@@ -0,0 +1,14 @@
+<?php
+
+  /**
+   * Contains all configurations
+   *
+   */
+   return [
+      'dbname' => 'leap_hk',
+      'pass' => '',
+      'username' => 'root',
+      'host' => 'localhost',
+    ];
+
+?>
